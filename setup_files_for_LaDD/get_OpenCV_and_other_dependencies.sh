9c298c2be5cf7fe4f681d9e6d582c25ff19d87fb
Kyle Nied <nied.kyle@gmail.com>
2018-05-24 21:35:51 -0400

    Update get_OpenCV_and_other_dependencies.sh

--- setup_files_for_LaDD/get_OpenCV_and_other_dependencies.sh
+++ setup_files_for_LaDD/get_OpenCV_and_other_dependencies.sh
@@ -1,2 +1,3 @@
 sudo apt-get install build-essential cmake cmake-curses-gui pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev \
-libeigen3-dev libxvidcore-dev libx264-dev libv4l-dev v4l-utils libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran python2-dev python2.7-dev python3-dev python3.5-dev python3-numpy python3-rpi.gpio numlockx -y
+libeigen3-dev libxvidcore-dev libx264-dev libv4l-dev v4l-utils libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran python2.7-dev python3-dev python3.5-dev python3-numpy \
+python3-rpi.gpio numlockx libgdk-pixbuf2.0-dev libpango1.0-dev libcairo2-dev libfontconfig1-dev -y
